81d9133a2143d5b666d043171a1cf7bdef4f9d72
Rahul K Murali <1620336@tcs.com>
2020-09-22 16:58:35 +0530

    Initial commit

---
+++ src/ios/AppCrash.m
@@ -0,0 +1,18 @@
+/********* AppCrash.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+
+@interface AppCrash : CDVPlugin {
+  // Member variables go here.
+}
+
+- (void)crash:(CDVInvokedUrlCommand*)command;
+@end
+
+@implementation AppCrash
+
+- (void)crash:(CDVInvokedUrlCommand *)command{
+    @[][1];
+}
+
+@end
